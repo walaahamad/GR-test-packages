b4b0d8ba9d4c03536f5d4597fd5ab1fd18a23f3e
walaahamad <33720328+walaahamad@users.noreply.github.com>
2017-11-21 14:19:32 +0200

    test 1

--- store/selectors.js
+++ store/selectors.js
@@ -1,37 +1,10 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-
-var getUi = exports.getUi = function getUi(_ref) {
-    var ui = _ref.ui;
-    return ui;
-};
-var getCategories = exports.getCategories = function getCategories(_ref2) {
-    var categories = _ref2.categories;
-    return categories;
-};
-var getCart = exports.getCart = function getCart(_ref3) {
-    var cart = _ref3.cart;
-    return cart;
-};
-var getNotifications = exports.getNotifications = function getNotifications(_ref4) {
-    var notifications = _ref4.notifications;
-    return notifications;
-};
-var getCheckout = exports.getCheckout = function getCheckout(_ref5) {
-    var checkout = _ref5.checkout;
-    return checkout;
-};
-var getForm = exports.getForm = function getForm(_ref6) {
-    var form = _ref6.form;
-    return form;
-};
-var getModals = exports.getModals = function getModals(_ref7) {
-    var modals = _ref7.modals;
-    return modals;
-};
+export const getUi = ({ui}) => ui
+export const getCategories = ({categories}) => categories
+export const getCart = ({cart}) => cart
+export const getNotifications = ({notifications}) => notifications
+export const getCheckout = ({checkout}) => checkout
+export const getForm = ({form}) => form
+export const getModals = ({modals}) => modals
